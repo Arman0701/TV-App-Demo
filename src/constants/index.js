df19431a4d1a88adfb454ce52a199e2ee25aabd1
Arman Tadevosyan <gittadevosyan889@gmail.com>
2025-08-02 00:30:32 +0400

    .env file support.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.VITE_BASE_URL || 'http://localhost:3000';
+export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
 
 export const LAST_SEEN_KEY = 'last_seen_ids';
